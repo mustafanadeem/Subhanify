1659b67eb88979ad47c22d87c1553a731d71aeee
ibnmaryamm <100576019+ibnmaryamm@users.noreply.github.com>
2025-10-20 09:12:59 +0100

    fix: remove extra quote in level-change-modal causing syntax error

--- components/level-change-modal.tsx
+++ components/level-change-modal.tsx
@@ -15,7 +15,7 @@ import {
 } from "react-native";
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
-const SCREEN_HEIGHT = Dimensions.get("window").height";
+const SCREEN_HEIGHT = Dimensions.get("window").height;
 
 interface LevelChangeModalProps {
   visible: boolean;
